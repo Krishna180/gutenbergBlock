d6bc969a50b486bf450bbaad859858960d31dc54
Krishna180 <dhongade.k@northeastern.edu>
2023-12-23 16:27:24 -0500

    Added a Formatting option in the Block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks'), 'version' => '40c71694e1f1c0490974');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => '93a2c05cca2234756631');
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import { registerBlockType } from '@wordpress/blocks';
+import { RichText } from '@wordpress/block-editor';
 
 registerBlockType('gutenberg-block/hello-world', {
    title: 'Hello World',
     icon: 'smiley',
     category: 'common',
-    edit: () => <div class = "wp-block-my-custom-block-hello-world">Hello World</div>
-    // save: () => <div class = "wp-block-my-custom-block-hello-world">A simple test block</div>,
-    
+    attributes: {
+        content: {
+            type: 'string',
+            source: 'html',
+            selector: 'div', // Using a div tag
+        },
+    },
+    edit: ({ attributes, setAttributes }) => {
+        return (
+            <RichText
+                tagName="div" // Using div instead of p
+                className="wp-block-my-custom-block-hello-world" // Custom class for styling
+                value={ attributes.content }
+                onChange={ ( content ) => setAttributes({ content }) }
+                placeholder="Hello World" // Default text
+            />
+        );
+    },
+    save: ({ attributes }) => {
+        return (
+            <RichText.Content
+                tagName="div"
+                className="wp-block-my-custom-block-hello-world"
+                value={ attributes.content }
+            />
+        );
+    },    
 });
 
 
